4f95c8423f81aeec9450890452f930a8202c7d1a
Shree <Shree@DESKTOP-8G3677I>
2024-09-23 07:39:42 +0530

    Add project files.

---
+++ CopyConstructor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+   
+    public class CopyConstructorExample
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        //Parametriesed constructor
+        public CopyConstructorExample(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        //copy constructor
+        public CopyConstructorExample(string name, int age)
+        {
+            NameNew = name;
+            AgeNew = age;
+        
+        
+        }
+           
+        public void DisplayInfo()
+        { 
+         Console.WriteLine($"Name: {Name}, Age:{Age}");
+        
+        }
+
+            
+    
+    
+    }
+
+    internal class CopyConstructor
+    {
+
+        static void Main()
+        {
+            //creating object with pass the value for parameterized constructor
+
+            CopyConstructorExample obj = new CopyConstructorExample("Akshay", 25);
+            obj.DisplayInfo();
+
+            //creating another object using copy constructor
+            CopyConstructorExample obj2 = new CopyConstructorExample();
+            obj2.DisplayInfo();
+        }
+
+    }
+
+}
---
+++ DefaultConstructor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class DefaultConstructorExample
+    {
+        public int value;
+        //default constructor
+        public DefaultConstructorExample()
+        {
+            value = 15;
+
+        }
+        // create another
+        public void Test()
+        {
+            Console.WriteLine("Test" + value);
+
+
+        }
+
+    }
+
+
+
+
+
+    internal class defaultConstructor
+    { 
+        static void Main()
+        {
+            DefaultConstructorExample obj = new DefaultConstructorExample();
+            Console.WriteLine(obj.value);
+            obj.Test();
+        }
+
+    }
+}
